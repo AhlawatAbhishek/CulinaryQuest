08500cce43fc3b8224b831bc51e47357429a73e0
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-07-23 13:42:42 +0530

    feat(MainViewModel): add suspend function call in viewModelScope coroutine

--- app/src/main/java/com/mayurappstudios/culinaryquest/MainViewModel.kt
+++ app/src/main/java/com/mayurappstudios/culinaryquest/MainViewModel.kt
@@ -1,5 +1,30 @@
 package com.mayurappstudios.culinaryquest
 
-class MainViewModel {
-    data class RecipeState(val loading: Boolean = true)
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class MainViewModel : ViewModel() {
+    private val _categoryState = mutableStateOf(RecipeState())
+    val categoryState: State<RecipeState> = _categoryState
+    private fun fetchCategories() {
+        viewModelScope.launch {
+            try {
+
+            } catch (e: Exception) {
+                _categoryState.value = _categoryState.value.copy(
+                    loading = false, error = "Error fetching recipe ${e.message}"
+                )
+            }
+        }
+    }
+
+    data class RecipeState(
+        val loading: Boolean = true,
+        val categories: List<Category> = emptyList(),
+        val error: String? = null
+    )
 }
